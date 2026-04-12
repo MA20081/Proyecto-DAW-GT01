f8a4a5b4aba8e6c3c6c225daecb1688b243fcbfd
Oscar Manuel Peraza <pv21001@gmail.com>
2026-04-12 15:38:12 -0600

    feat: entidad Cita y script schema.sql hecho por mi persona, Oscar Peraza

---
+++ backend/mediagenda/database/schema.sql
@@ -0,0 +1,12 @@
+-- Script de creación de tabla principal: citas
+-- Proyecto: MediAgenda
+-- Autor: Oscar Manuel Peraza Velasquez - PV21001
+
+CREATE TABLE citas (
+    id               SERIAL PRIMARY KEY,
+    nombre_paciente  VARCHAR(100) NOT NULL,
+    nombre_doctor    VARCHAR(100) NOT NULL,
+    especialidad     VARCHAR(80)  NOT NULL,
+    fecha_hora       TIMESTAMP    NOT NULL,
+    estado           VARCHAR(20)  DEFAULT 'PENDIENTE'
+);
